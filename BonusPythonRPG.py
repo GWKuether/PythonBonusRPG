c4f8b120a92b4c9c7f988d97ce09bc5fe3210074
George Kuether <gwkuether@gmail.com>
2022-10-26 16:51:19 -0500

    Laying out basics and learning about dictionaries

--- BonusPythonRPG.py
+++ BonusPythonRPG.py
@@ -1,1 +1,13 @@
-test = "test"
+
+
+
+Hercules = {'Name': "Hercules", 'Health': 100, 'Attack Power': 50, 'Attacks': ['Punch', 'Kick', 'Tackle', 'Trample']}
+
+Gremlin_1 = {'Health': 20, 'Attack Power': 5, 'Attacks': ['Bite', 'Hiss', 'Scratch']}
+
+Gremlin_2 = {'Health': 20, 'Attack Power': 5, 'Attacks': ['Bite', 'Hiss', 'Scratch']}
+
+Dragon = {'Health': 200, 'Attack Power': 25, 'Attacks': ['Breathe Fire', 'Tail Slap' 'Slash']}
+
+
+print(Hercules['Attacks'][2])
