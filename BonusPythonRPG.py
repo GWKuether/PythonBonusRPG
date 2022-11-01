5f13dacbada773a08a7bf374d54f5877724172a8
George Kuether <gwkuether@gmail.com>
2022-11-01 14:46:03 -0500

    Added a loop to allow selection of attack from a dictionary list

--- BonusPythonRPG.py
+++ BonusPythonRPG.py
@@ -1,4 +1,4 @@
-
+import random
 
 
 Hercules = {'Name': "Hercules", 'Health': 100, 'Attack Power': 50, 'Attacks': ['Punch', 'Kick', 'Tackle', 'Trample']}
@@ -11,3 +11,42 @@ Dragon = {'Health': 200, 'Attack Power': 25, 'Attacks': ['Breathe Fire', 'Tail S
 
 
 print(Hercules['Attacks'][2])
+
+gremlin_1_attack = random.choice(Gremlin_1['Attacks'])
+
+gremlin_2_attack = random.choice(Gremlin_1['Attacks'])
+
+print(gremlin_1_attack)
+
+print(gremlin_2_attack)
+
+
+
+
+def choose_hercules_attack(enemy):
+    attack_chosen = False
+    while attack_chosen == False:
+        attack_choice = input(f"Which attack should Hercules use? {Hercules['Attacks'][0]}, {Hercules['Attacks'][1]}, {Hercules['Attacks'][2]}, or {Hercules['Attacks'][3]}? ")
+        if attack_choice == Hercules['Attacks'][0]:
+            print(f"Hercules used his giant hand to punch the {enemy}.")
+            attack_chosen = True
+        elif attack_choice == Hercules['Attacks'][1]:
+            print(f"Hercules wound up his leg and kicked the {enemy}.")
+            attack_chosen = True
+        elif attack_choice == Hercules['Attacks'][2]:
+            print(f"Hercules flew into the air and tackled the {enemy}.")
+            attack_chosen = True
+        elif attack_choice == Hercules['Attacks'][3]:
+            print(f"Hercules lifted his boots and trampled the {enemy}.")
+            attack_chosen = True
+
+
+
+choose_hercules_attack("Dragon")
+
+
+def run_game():
+    pass
+
+def fight(initiator, receiver):
+    pass
